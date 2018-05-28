09cb62da4384661f5b144dc908c0180af309b304
Faizul Nordin <norfaizulizwan.nordin@experian.com>
2018-05-28 15:49:47 +0800

    Update baru

--- js/update.js
+++ js/update.js
@@ -1,7 +1,7 @@
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "asdf" && password == "asdf"){
+if ( username == "asdf@gmail.com" && password == "asdf"){
 window.location = "done.html"; // Redirecting to other page.
 return false;
 }else{
